864c60a8a5dad878a4e26272f8eb645c166f5931
Enzo Brincalepe <enzo.brincalepe@gmail.com>
2025-04-17 22:06:47 -0300

    arrumando o banner

--- src/js/script.js
+++ src/js/script.js
@@ -2,4 +2,14 @@ document.querySelector("form").onsubmit = function(e) {
   e.preventDefault();
   alert("Mensagem enviada com sucesso!");
 };
-var idade = prompt("Qual a sua idade atual? ")
+
+let slideIndex = 0;
+  const slides = document.querySelector('.slides');
+  const totalSlides = document.querySelectorAll('.slides img').length;
+
+  function mostrarProximoSlide() {
+    slideIndex = (slideIndex + 1) % totalSlides;
+    slides.style.transform = `translateX(-${slideIndex * 100}%)`;
+  }
+
+  setInterval(mostrarProximoSlide, 3000); // troca a cada 3 segundos
